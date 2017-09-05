05ce8446af0fc41bd23e50375f3b3d66d41b4792
RCosson <romain.cosson11@live.fr>
2017-09-05 15:47:28 +0200

    content added

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 	$(".carousel-top").slick({
+		nextArrow: '<i class="fa fa-chevron-right" aria-hidden="true"></i>',
+  		prevArrow: '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
 		slidesToScroll: 1,
 		autoplay: true,
-		autoplaySpedd: 2000,
+		autoplaySpeed: 5000,
 		dots: true,
 	});
 });
