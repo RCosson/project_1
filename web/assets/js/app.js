fc67f8b00eeb2091c2046b066d1e5a349121c328
RCosson <romain.cosson11@live.fr>
2017-09-06 12:21:42 +0200

    top page front progression

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
 		nextArrow: '<img class="next-arrow" src="assets/img/top-carousel/fleche_slide_droite.png">',
   		prevArrow: '<img class="prev-arrow" src="assets/img/top-carousel/fleche_slide_gauche.png">',
 		slidesToScroll: 1,
-		/*autoplay: true,
-		autoplaySpeed: 5000,*/
+		autoplay: true,
+		autoplaySpeed: 5000,
 		dots: true,
 	});
 	$(".top-info-carousel").slick({
