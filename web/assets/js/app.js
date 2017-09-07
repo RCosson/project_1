6ad47ae55112c3d37d2b0e410c47e775481a4929
RCosson <romain.cosson11@live.fr>
2017-09-07 18:32:05 +0200

    third carousel almost finished

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
 		slidesToScroll: 1,
  		arrows: false,
  		dots: false,
- 		fade: true,
+ 		speed: 250,
  		asNavFor: '.slider-nav',
 	});
 	$('.slider-nav').slick({
@@ -28,35 +28,5 @@ $(document).ready(function(){
 		asNavFor: '.middle-info-carousel',
  		dots: true,
  		focusOnSelect: true,
-	});
-	$( ".nav-slide" ).click(function() {
-	if ($(".nav-slide1").hasClass("slick-current")) {
-		$("#entr").prop("src","assets/img/bottom-carousel/picto_les_entreprises_2.png");
-		
-
-	} else if ($(".nav-slide2").hasClass("slick-current")) {
-		$("#com").prop("src","assets/img/bottom-carousel/picto_centres_commerciaux_2.png");
-		if (!$(".nav-slide2").hasClass("slick-current")) {
-			$("#com").prop("src","assets/img/bottom-carousel/picto_centres_commerciaux.png");
-		}
-
-	} else if ($(".nav-slide3").hasClass("slick-current")) {
-		$("#collec").prop("src","assets/img/bottom-carousel/picto_collectivites_2.png");
-		if (!$(".nav-slide3").hasClass("slick-current")) {
-			$("#collec").prop("src","assets/img/bottom-carousel/picto_collectivites.png");
-		}
-
-	} else if ($(".nav-slide4").hasClass("slick-current")) {
-		$("#ecoles").prop("src","assets/img/bottom-carousel/picto_ecoles_2.png");
-		if (!$(".nav-slide4").hasClass("slick-current")) {
-			$("#ecoles").prop("src","assets/img/bottom-carousel/picto_ecoles.png");
-		}
-
-	} else if ($(".nav-slide5").hasClass("slick-current")) {
-		$("#parents").prop("src","assets/img/bottom-carousel/picto_parents_2.png");
-		if (!$(".nav-slide5").hasClass("slick-current")) {
-			$("#parents").prop("src","assets/img/bottom-carousel/picto_parents.png");
-		}
-	}
 	});
 });
